b3864653d418bc7a65fa5ea0822f7bc1b06f1c69
baelsxbs <sebastiand.martinezo@konradlorenz.edu.co>
2023-09-11 00:23:44 -0500

    se cre un parametro domde si da como resultado cero se identifica como un año bisiesto

---
+++ biciesto.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Bisiesto {
+
+    public static boolean esBisiesto(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un año: ");
+        int year = scanner.nextInt();
+
+        if (esBisiesto(year)) {
+            System.out.println(year + " es un año bisiesto.");
+        } else {
+            System.out.println(year + " no es un año bisiesto.");
+        }
+    }
+}
